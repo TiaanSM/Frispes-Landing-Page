221f1598966bdea3932d10d0e99c115886003ff9
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-09-29 14:05:12 +0200

    Improved overall responsiveness.

--- src/main.js
+++ src/main.js
@@ -4,13 +4,3 @@ navHamburger.addEventListener('click', () => {
 	const mobileMenu = document.querySelector('.mobile-menu');
 	mobileMenu.classList.toggle('active-menu');
 });
-
-
-
-const awnser = document.querySelector('.awnser-icon');
-
-awnser.addEventListener('mouseover', () => {
-    const question = document.querySelector('.question');
-
-	question.textContent = 'Check the form above';
-});
