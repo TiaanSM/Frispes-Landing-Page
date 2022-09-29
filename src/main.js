772b0ca1f55b733254349f090245dc03cdf40fb9
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-09-29 20:42:15 +0200

    Planned out the js required for animations

--- src/main.js
+++ src/main.js
@@ -4,3 +4,37 @@ navHamburger.addEventListener('click', () => {
 	const mobileMenu = document.querySelector('.mobile-menu');
 	mobileMenu.classList.toggle('active-menu');
 });
+
+
+// Test to see if images fit,(need to clean up the code to a for each click statement)
+const itemOne = document.getElementById('fac-1');
+const itemTwo = document.getElementById('fac-2');
+const itemThree = document.getElementById('fac-3');
+const itemFour = document.getElementById('fac-4');
+const itemFive = document.getElementById('fac-5');
+
+
+itemOne.addEventListener('click', () => {
+   const facImage1 = document.querySelector('.fac-image1');
+   facImage1.classList.toggle('fac-image1');
+});
+
+itemTwo.addEventListener('click', () => {
+	const facImage2 = document.querySelector('.fac-image2');
+	facImage2.classList.toggle('fac-image2');
+});
+
+itemThree.addEventListener('click', () => {
+	const facImage3 = document.querySelector('.fac-image3');
+	facImage3.classList.toggle('fac-image3');
+});
+
+itemFour.addEventListener('click', () => {
+	const facImage4 = document.querySelector('.fac-image4');
+	facImage4.classList.toggle('fac-image4');
+});
+
+itemFive.addEventListener('click', () => {
+	const facImage5 = document.querySelector('.fac-image5');
+	facImage5.classList.toggle('fac-image5');
+});
